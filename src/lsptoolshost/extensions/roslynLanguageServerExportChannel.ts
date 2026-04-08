2e3af5de4a18b3752f4feeacd34651e8901c3d6e
David Barbet <dabarbet@microsoft.com>
2026-04-07 19:07:30 -0700

    Use new protocol method for sg content and normalize language client imports

--- src/lsptoolshost/extensions/roslynLanguageServerExportChannel.ts
+++ src/lsptoolshost/extensions/roslynLanguageServerExportChannel.ts
@@ -3,22 +3,21 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import * as vscode from 'vscode';
-import { RequestType } from 'vscode-jsonrpc';
 import { RoslynLanguageServer } from '../server/roslynLanguageServer';
-import { PartialResultParams, ProtocolRequestType } from 'vscode-languageserver-protocol';
+import { PartialResultParams, ProtocolRequestType, RequestParam, RequestType } from 'vscode-languageclient';
 
 export class RoslynLanguageServerExport {
     constructor(private _serverInitialized: Promise<RoslynLanguageServer>) {}
 
     public async sendRequest<Params, Response, Error>(
         type: RequestType<Params, Response, Error>,
-        params: Params,
+        params: RequestParam<Params>,
         token: vscode.CancellationToken
     ): Promise<Response> {
         const server = await this._serverInitialized;
         // We need to recreate the type parameter to ensure that the prototypes line up. The `RequestType` we receive could have been
         // from a different version.
-        const newType = new RequestType<Params, Response, Error>(type.method);
+        const newType = new RequestType<Params, Response, Error>(type.method, type.parameterStructures);
         return await server.sendRequest(newType, params, token);
     }
 
@@ -30,7 +29,7 @@ export class RoslynLanguageServerExport {
         RegistrationOptions
     >(
         type: ProtocolRequestType<Params, Response, PartialResult, Error, RegistrationOptions>,
-        params: Params,
+        params: RequestParam<Params>,
         onProgress: (p: PartialResult) => Promise<any>,
         token?: vscode.CancellationToken
     ): Promise<Response> {
